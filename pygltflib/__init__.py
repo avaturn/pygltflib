87ec1eb2ee44e69c14c41114b69485bbc180005b
Luke Miller <dodgyville@gmail.com>
2021-09-20 14:14:23 +1000

    bump version number

--- pygltflib/__init__.py
+++ pygltflib/__init__.py
@@ -47,7 +47,7 @@ from dataclasses_json.core import _decode_dataclass
 from dataclasses_json.core import _ExtendedEncoder as JsonEncoder
 from deprecated import deprecated
 
-__version__ = "1.14.4"
+__version__ = "1.14.5"
 
 """
 About the GLTF2 file format:
